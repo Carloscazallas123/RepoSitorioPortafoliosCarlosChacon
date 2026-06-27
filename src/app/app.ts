2a4cd71951433263cb31cc23f0bc1af72fe8e63c
Carloscazallas123 <carloschaconcazallas0@gmail.com>
2026-06-27 16:27:49 +0200

    Agregar 'ComponenteBanner' con su HTML, CSS y pruebas unitarias

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ComponenteBanner } from "./components/componente-banner/componente-banner";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ComponenteBanner],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/componente-banner/componente-banner.ts
+++ src/app/components/componente-banner/componente-banner.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-componente-banner',
+  selector: 'ComponenteBanner',
   imports: [],
   templateUrl: './componente-banner.html',
   styleUrl: './componente-banner.css',
 })
-export class ComponenteBanner {}
+export class ComponenteBanner {
+
+    constructor(){}
+}
